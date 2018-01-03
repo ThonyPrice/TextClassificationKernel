22e44276b24a245f9bd9eb18a82fd98602e61c27
Thony Price <thonyprice@gmail.com>
2018-01-03 14:31:45 +0100

    Initial commit

---
+++ WK.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+WK.py
+
+This class provides conversion of text document into a feature vector
+to be used in an SVM for text classification purposes.
+"""
+
+class WK(object):
+
+    def __init__(self):
+        pass
