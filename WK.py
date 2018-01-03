094f80231be611d793022563ce3adcc17a02a271
Thony Price <thonyprice@gmail.com>
2018-01-03 15:41:19 +0100

    reading file work's OK - onward with stop words removal

--- WK.py
+++ WK.py
@@ -7,15 +7,23 @@ This class provides conversion of text document into a feature vector
 to be used in an SVM for text classification purposes.
 """
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 class WK(object):
 
     def __init__(self, doc_path):
         file_content = self.openFile(doc_path)
+        print(file_content)
+        processed_text = self.rmStopWords(file_content)
 
     def openFile(self, path):
-        return
+        with open(path, 'r', encoding='utf-8') as myfile:
+            text = myfile.read()
+        return text
 
+    def rmStopWords(self, text):
+        return
 
 # Currently osed for testing purposes,
 # remove main() when done with testing.
