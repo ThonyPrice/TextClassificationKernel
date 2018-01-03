373b97f2fea935d13eb3ea61c9e4f4f64ae96f29
Thony Price <thonyprice@gmail.com>
2018-01-03 14:54:28 +0100

    added main for test purposes

--- WK.py
+++ WK.py
@@ -7,7 +7,22 @@ This class provides conversion of text document into a feature vector
 to be used in an SVM for text classification purposes.
 """
 
+
 class WK(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, doc_path):
+        file_content = self.openFile(doc_path)
+
+    def openFile(self, path):
+        return
+
+
+# Currently osed for testing purposes,
+# remove main() when done with testing.
+def main():
+    path = 'test_docs/alice_ch1.txt'
+    result = WK(path)
+    return
+
+if __name__ == '__main__':
+    main()
