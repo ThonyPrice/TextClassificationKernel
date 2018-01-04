caa8815b71aee7e7c70dbc0c2a48be86c3932b1c
Thony Price <thonyprice@gmail.com>
2018-01-04 10:59:57 +0100

    Changed calculations of feature vector to more correct ones according to report

--- WK.py
+++ WK.py
@@ -4,43 +4,70 @@
 # TODO: Include NLTK packages to git repository
 # TODO: Investigate if it's reasonable to perform stemming on dataset
 
-import numpy as np
-import os
 from collections import Counter
+import itertools
+from math import log
+import os
+
+import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+
 '''
-This class provides conversion of text document into a feature vector
-to be used in an SVM for text classification purposes. Initiate class with
-a path to a file which should be convertet to a feature vectore for the
-Word Kernel (WK). The feature vector can be collected immedeatly as:
+This class provides conversion of text documents into a feature vector
+to be used in an SVM for text classification purposes. Initiate class
+with a path to a directory with documents that should be convertet to a
+feature vector for the Word Kernel (WK). The feature vector can be
+collected immedeatly as:
 
 >>> WK(/path/to/file).featureVector
 
+Notice (dependencies):
+The current version requires NLTK (use pip install),
+decuments must be .txt files (easily modified),
+document encoding must be UTF-8.
 '''
+
+
 class WK(object):
 
     def __init__(self, directory):
-        docs = self.filesFrom(directory))
-        for document in docs:
-            file_content = self.open(document)
-            processed_text_list = self.rmStopWords(file_content)
-        self.featureVector = self.featureVector(processed_text_list)
+        docs = self.filesFrom(directory)
+        n = len(docs)
+        content_list = self.extract(docs)
+        content_list = self.process(content_list)
+        separate_doc_counts = [Counter(content) for content in content_list]
+        total_count = Counter(list(itertools.chain.from_iterable(content_list)))
+        vector = [
+            log(1 + total_count[x])
+            # Sum below is document frequency
+            * (n / sum([1 for count in separate_doc_counts if x in count]))
+            for x in total_count
+            if total_count[x] > 3
+        ]
+        self.featureVector = np.asarray(vector)
 
     def filesFrom(self, directory):
         return [
-            file_name for file_name in os.listdir(directory)
+            directory + file_name
+            for file_name in os.listdir(directory)
             if file_name.endswith(".txt")
         ]
+
+    def extract(self, docs):
+        return [self.open(document) for document in docs]
 
     def open(self, path):
         with open(path, 'r', encoding='utf-8') as myfile:
             return myfile.read()
 
+    def process(self, docs):
+        return [self.rmStopWords(document) for document in docs]
+
     def rmStopWords(self, text):
         signs = [
-            '.', ',', ':', ';', '-', '?',
+            '.', ',', ':', ';', '-', '?', '--'
             '!', "'", '(', ')', '[', ']'
         ]
         stop_words = set(stopwords.words('english'))
@@ -61,7 +88,7 @@ class WK(object):
 # remove main() when done with testing.
 def main():
     path = './test_docs/'
-    result = WK(path).vector
+    result = WK(path).featureVector
     print('Result: ', result)
     print('~*~ End Of Word Kernel ~*~')
 
