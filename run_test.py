2c4e6a2173af1bd442234b615bb81bf11fd28bef
Eduardo <ehm@kth.se>
2018-01-12 09:39:46 +0100

    Added ploting function comparing classifiers. Run several lengths.

--- run_test.py
+++ run_test.py
@@ -10,18 +10,19 @@ import reut as r
 import test as t
 import nltk
 from nltk.corpus import reuters
+import plot as pt
 
-def play(i1, i2, list):
+def play(ngram, list):
 	config = {}
 	train, test, docs = r.load_docs(config)
 	# Obtain WK processed documents	
 	xs_wk = r.WK(docs)
 	# Obtain NGK processed documents
-	xs_ngk = r.NGK(docs, ngram)
+	# xs_ngk = r.NGK(docs, ngram)
 	ys = r.targets(train, test)
 	# put xs and ys together for each case
 	data_wk = r.data(xs_wk, ys)
-	data_ngk = r.data(xs_ngk, ys)
+	# data_ngk = r.data(xs_ngk, ys)
 	# Get index from desired categories  
 	listNum = []
 	for i in list:
@@ -29,5 +30,11 @@ def play(i1, i2, list):
 			if i == reuters.categories()[w]:
 				listNum += [w]
 	# Testing
-	t.test(data_wk, list, listNum, "WK")
-	t.test(data_ngk, list, listNum, "NGK")
+	for j in range(len(ngram)):
+		xs_ngk = r.NGK(docs, ngram[j])
+		data_ngk = r.data(xs_ngk, ys)
+		name = "NGK length "+str(ngram[j])	
+		t.test(data_ngk, list, listNum, name) 
+
+	n, f, p, a = t.test(data_wk, list, listNum, "WK")	
+	pt.plot(n, f, p, a)
--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import SVC
-from sklearn.svm import LinearSVC
-from sklearn.metrics import classification_report
-from sklearn.metrics import f1_score
-from sklearn.metrics import precision_score
+from sklearn.svm import SVC, LinearSVC
+from sklearn.metrics import f1_score, precision_score, fbeta_score, classification_report, accuracy_score
 from sklearn import linear_model
 import nltk
 from nltk.corpus import reuters
 import time
+
 # Here we import SKK kernel file. 
 # import SKKpyfile as SKK
+
+p, a, f, clf = [], [], [], []
 
 def test(data, listLabels, listNum, k):
 	data = data
@@ -19,20 +19,30 @@ def test(data, listLabels, listNum, k):
 
 	# Classifiers to use with the data	
 	classifiers = [
-			(k + ' LinearSVC', OneVsRestClassifier(LinearSVC(random_state=42))),
-			,(k + ' SVM SVC linear', OneVsRestClassifier(SVC(kernel="linear", cache_size=200, random_state=42)))
+			(k + ' LinearSVC', OneVsRestClassifier(LinearSVC(random_state=42)))
+			#,(k + ' SVM SVC linear', OneVsRestClassifier(SVC(kernel="linear", cache_size=200, random_state=42)))
 			#,(k + 'SKK kernel', OneVsRestClassifier(SVC(kernel=SKK.kernel(data), cache_size=200, random_state=42, decision_function_shape='ovr')))
-			#,(k + 'Linear with SGD', OneVsRestClassifier(linear_model.SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42)))
+			#,(k + 'Linear with SGD', OneVsRestClassifier(linear_model.SGDClassifier(penalty='l2',alpha=0.001, random_state=42, max_iter=100)))
 			]
 
-	for clf_name, classifier in classifiers:
+	for index in range(len(classifiers)):
+		clf_name, classifier = classifiers[index]	
 		t0 = time.time()
 		classifier.fit(xs['train'], ys['train'])
 		t1 = time.time()
 		preds = classifier.predict(data['x_test'])
+		preds[preds >= 0.5] = 1
+		preds[preds < 0.5] = 0
 		t2 = time.time()
 		y_true = ys['test']
 		y_pred = preds
+		prec = precision_score(y_true, y_pred, average='weighted')
+		f1 = fbeta_score(y_true, y_pred, beta=1, average='weighted')
+		acc = accuracy_score(y_true, y_pred)
+		p.append(prec), f.append(f1), a.append(acc), clf.append(clf_name) 
+		print("classifier: ",clf_name,"\tf1: ",f1,"\tprecision: ",prec,"\taccuracy: ",acc)
 		print("-"*20,("{clf_name:<16}").format(clf_name=clf_name),"-"*20)
 		# Classification report for each classifier	
 		print(classification_report(y_true, y_pred, target_names=listLabels, labels=listNum))
+
+	return clf, f, p, a
