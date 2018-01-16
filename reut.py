1a59ddfdbbfe943e81da4887e34c0f06f3eaa20c
Erik Söderberg <esoderb@kth.se>
2018-01-16 11:33:07 +0100

    Trying to fix it so git doesn't keep flagging newline characters

--- reut.py
+++ reut.py
@@ -18,10 +18,10 @@ def load_docs(label=None, config={}):
 	docs['train'] = [reuters.raw(doc_id) for doc_id in train]
 	docs['test'] = [reuters.raw(doc_id) for doc_id in test]
 	return train, test, docs
-	
+
 #WK documents processing
 def WK(docs):
-	vectorizer = CountVectorizer(stop_words=stop_words)	
+	vectorizer = CountVectorizer(stop_words=stop_words)
 	tfidf_transformer = TfidfTransformer()
 	xs = {'train': [], 'test': []}
 	xs['train'] = vectorizer.fit_transform(docs['train'])
@@ -35,7 +35,7 @@ def WK(docs):
 # param ngram_size is the grams to be considered. (Sequence length)
 def NGK(docs, ngram_size):
 	vectorizer = CountVectorizer(stop_words=stop_words, ngram_range = (1, ngram_size))
-	tfidf_transformer = TfidfTransformer()	
+	tfidf_transformer = TfidfTransformer()
 	xs = {'train': [], 'test': []}
 	xs['train'] = vectorizer.fit_transform(docs['train'])
 	xs['test'] = vectorizer.transform(docs['test'])
@@ -43,14 +43,14 @@ def NGK(docs, ngram_size):
 	xs_tfidf['train'] = tfidf_transformer.fit_transform(xs['train'])
 	xs_tfidf['test'] = tfidf_transformer.transform(xs['test'])
 	return xs_tfidf
-	
+
 def targets(train, test):
 	mlb = MultiLabelBinarizer()
 	ys = {'train': [], 'test': []}
 	ys['train'] = mlb.fit_transform([reuters.categories(doc_id) for doc_id in train])
 	ys['test'] = mlb.transform([reuters.categories(doc_id) for doc_id in test])
 	return ys
-	
+
 def data(xs,ys):
 	data = {'x_train': xs['train'], 'y_train': ys['train'],'x_test': xs['test'], 'y_test': ys['test'],'labels': globals()["labels"]}
 	return data
